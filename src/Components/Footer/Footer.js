be5e7a89500a01360a0c857cc4e590d4089d767d
abdullah-ch <abdullahchaudharry@gmail.com>
2020-09-25 19:19:01 +0500

    added footer icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Footer.module.css";
@@ -6,10 +9,50 @@ import styles from "./Footer.module.css";
 const CustomFooter = () => {
   return (
     <div className={styles.footer}>
+      <a
+        href="https://www.facebook.com/mrabdullahch/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span size="30px" className="icon">
+          <FaFacebook />
+        </span>
+      </a>
+
+      <a
+        href="https://www.instagram.com/mrabdullahch/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="icon">
+          <GrInstagram />
+        </span>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/abdullah-ch/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="icon">
+          <AiFillLinkedin />
+        </span>
+      </a>
+
+      <a
+        href="https://github.com/abdullah-ch"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="icon">
+          <AiFillGithub />
+        </span>
+      </a>
+
       <p class="love">
-        Made with <FontAwesomeIcon color="red" icon={faHeart} /> by{" "}
+        Made with <FontAwesomeIcon color="red" icon={faHeart} /> by
         <span
-          className={styles.span}
+          className={styles.spann}
           onClick={() =>
             (window.location.href = "https://github.com/abdullah-ch")
           }
