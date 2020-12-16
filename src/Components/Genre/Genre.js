f6d5d9fd368a0aa74e70645ae0c506a8ee04a519
abdullah-ch <abdullahchaudharry@gmail.com>
2020-12-16 17:43:26 +0500

    styled genre menu

--- src/Components/Genre/Genre.js
+++ src/Components/Genre/Genre.js
@@ -2,28 +2,51 @@ import React from "react";
 import styles from "./Genre.module.css";
 import { genres } from "../../api";
 import { useHistory } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const Genre = () => {
   let history = useHistory();
   return (
     <div className={styles.gridContainer}>
       {genres.map((genre, i) => (
-        <h3
-          className={styles.gridItem}
-          onClick={(event) => {
-            event.preventDefault();
-            const genreID = ++i;
-            history.push({
-              pathname: `/anime/genre/${genre}`,
-              state: {
-                genreID: genreID,
-              },
-            });
-            window.location = `/anime/genre/${genre}`;
-          }}
-        >
-          {genre}
-        </h3>
+        <div>
+          {/* <h3
+            className={styles.gridItem}
+            onClick={(event) => {
+              event.preventDefault();
+              const genreID = ++i;
+              history.push({
+                pathname: `/anime/genre/${genre}`,
+                state: {
+                  genreID: genreID,
+                },
+              });
+              window.location = `/anime/genre/${genre}`;
+            }}
+          >
+            {genre}
+          </h3> */}
+          <Card className={styles.genreCard}>
+            <Card.Body>
+              <Card.Title
+                className={styles.gridItem}
+                onClick={(event) => {
+                  event.preventDefault();
+                  const genreID = ++i;
+                  history.push({
+                    pathname: `/anime/genre/${genre}`,
+                    state: {
+                      genreID: genreID,
+                    },
+                  });
+                  window.location = `/anime/genre/${genre}`;
+                }}
+              >
+                {genre}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </div>
       ))}
     </div>
   );
