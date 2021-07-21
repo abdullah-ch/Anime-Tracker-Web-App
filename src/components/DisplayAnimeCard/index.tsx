59daab1a092c012aad7b9e6aae6039c200fabeab
M.Abdullah Ch <abdullahchaudharry@gmail.com>
2021-07-21 23:16:47 +0500

    hover effect added

--- src/components/DisplayAnimeCard/index.tsx
+++ src/components/DisplayAnimeCard/index.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Typography, Grid } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 
 const styles = makeStyles((theme) => ({
-    root: {
+    card: {
         background: 'black',
-        width: '142px',
+        width: '140px',
         margin: '0px 10px',
-        transition: 'all .4s ease-out',
-        '&:hover': {
-            '&::after': {
-                width: '100%',
-            },
-        },
-        '&::after': {
-            content: 'none',
-            height: '100%',
-            backgroundColor: theme.palette.primary.main,
-        },
     },
-    image: {
+
+    root: {
         width: '142px',
         height: '196px',
+        overflow: 'hidden',
+        position: 'relative',
+        transition: 'all 0.5s ease-out',
+    },
+    image: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
         objectFit: 'cover',
+        transition: 'all 0.5s ease-out',
+        zIndex: 10,
+        '&:hover': {
+            zIndex: -1,
+        },
     },
     textPositioning: {
         width: '140px',
@@ -36,11 +39,25 @@ const styles = makeStyles((theme) => ({
         alignItems: 'center',
         fontFamily: 'Comfortaa',
     },
+
+    genresContainer: {
+        backgroundColor: theme.palette.primary.main,
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        transition: 'all 0.5s ease-out',
+        zIndex: 2,
+    },
+    genreList: {
+        width: '100%',
+        height: '100%',
+    },
 }));
 
 interface AnimeData {
     image_url: string;
-    title: String;
+    title: string;
+    genres: Array<any>;
 }
 interface DisplayAnimeCardProps {
     data: AnimeData;
@@ -51,10 +68,34 @@ const DisplayAnimeCard: React.FC<DisplayAnimeCardProps> = (
 ) => {
     const classes = styles();
     const { data } = props;
-    console.log(typeof data, data.image_url);
+    console.log(typeof data, data.genres);
     return (
-        <Card className={classes.root}>
-            <img className={classes.image} alt='anime' src={data.image_url} />
+        <Card className={classes.card}>
+            <div className={classes.root}>
+                <img
+                    className={classes.image}
+                    alt='anime'
+                    src={data.image_url}
+                />
+                <div className={classes.genresContainer}>
+                    <Grid
+                        className={classes.genreList}
+                        container
+                        direction='column'
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        {data.genres.map((genre) => (
+                            <Typography
+                                className={classes.text}
+                                variant='body1'
+                            >
+                                {genre.name}
+                            </Typography>
+                        ))}
+                    </Grid>
+                </div>
+            </div>
             <div className={classes.textPositioning}>
                 <Typography className={classes.text} variant='body1'>
                     {data.title}
